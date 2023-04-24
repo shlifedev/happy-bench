57f1ccceb960584ea938228be11c2f5f485303ef
shlifedev <shlifedev@gmail.com>
2023-04-24 14:42:34 +0900

    pkgMeta

--- unity/asset-extractor/export.ps1
+++ unity/asset-extractor/export.ps1
@@ -1,6 +1,13 @@
 $assetPath = ".\asset";
 $items = Get-ChildItem -Path $assetPath
 
+
+class PkgMeta{
+  [string] $originalFileName;
+  [string] $assetstoreUrl; 
+  [string] $type;  
+}
+
 foreach($item in $items){
   Write-Output $item
   if($item.Extension -eq ".unitypackage"){ 
@@ -9,4 +16,15 @@ foreach($item in $items){
       New-Item -Path $output -ItemType Directory -Force
       tar xzvf $input -C $output
   } 
+}
+ 
+function Write-ExtractPkg {
+  param(
+     [string] $Path,
+     [string] $OutputPath
+  ) 
+}
+
+function Read-PkgMeta{
+
 }
